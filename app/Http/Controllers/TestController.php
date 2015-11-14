a701b454aac3aa0b3cb33ad1dab417d33527d966
sbezz <bezz.sergio@gmail.com>
2015-11-14 12:25:22 -0200

    - Adicionando novos arquivos nos diretório view e public.

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class TestController extends Controller
+{
+    public function index($nome)
+    {
+        //
+        //Criando a váriavel $nome
+       // $nome = "Sergio";
+        return "Olá $nome";
+
+
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show($id)
+    {
+        //
+    }
+
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
